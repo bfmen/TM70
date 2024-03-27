46bd715d44408e383de81b03ba99ea5dcb0e3cc7
bfmen <62467909+bfmen@users.noreply.github.com>
2024-03-27 11:52:43 +0800

    Update JC09-8080.sh

--- JC09-8080.sh
+++ JC09-8080.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /srv
 mount -o remount,rw /
-curl -Lo "8080.zip" http://op.qulenali.cn:7890/static/media/8080.zip
+curl -Lo "8080.zip" https://gh-proxy.com/https://raw.githubusercontent.com/bfmen/TM70/main/8080.zip
 unzip -o 8080.zip  -d www
 rm -rf 8080.zip
 echo "操作完成"
